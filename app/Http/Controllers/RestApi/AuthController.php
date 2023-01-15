0253f8a5f35d2d857bfe53bfd775bebc3b29fdb0
Sydney G. Kibanga <51691556+sydney2323@users.noreply.github.com>
2023-01-15 08:24:43 +0300

    Update AuthController.php

--- app/Http/Controllers/RestApi/AuthController.php
+++ app/Http/Controllers/RestApi/AuthController.php
@@ -92,51 +92,51 @@ class AuthController extends Controller
 		}
     }
 
-	public function adminLogin(Request $request)
-	{
-		$input = $request->all();
-		$validator = Validator::make($input, [
-			'email' => 'required|email',
-			'password' => 'required',
-		]);
-		if ($validator->fails()) {
+// 	public function adminLogin(Request $request)
+// 	{
+// 		$input = $request->all();
+// 		$validator = Validator::make($input, [
+// 			'email' => 'required|email',
+// 			'password' => 'required',
+// 		]);
+// 		if ($validator->fails()) {
 			
-			return response()->json($validator->errors(), 417);
-		}
-		$credentials = $request->only(['email', 'password']);
-		if (Auth::attempt($credentials)) {
+// 			return response()->json($validator->errors(), 417);
+// 		}
+// 		$credentials = $request->only(['email', 'password']);
+// 		if (Auth::attempt($credentials)) {
 			
-			$user = Auth::user();
-			$success['token'] = $user->createToken('MyApp', ['*'])->accessToken;
-			return response()->json(['success' => $success], 200);
-		}
-		else {
-			return response()->json(['error' => 'Unauthorized'], 401);
-		}
-	}
-	/**
-	 * admin register API
-	 * @return \Illuminate\Http\Response
-	 */
-	public function adminRegister(Request $request)
-	{
-		$validator = Validator::make($request->all(), [
-			'name' => 'required',
-			'email' => 'required|email',
-			'password' => 'required',
-			'c_password' => 'required|same:password',
-		]);
-		if ($validator->fails()) {
+// 			$user = Auth::user();
+// 			$success['token'] = $user->createToken('MyApp', ['*'])->accessToken;
+// 			return response()->json(['success' => $success], 200);
+// 		}
+// 		else {
+// 			return response()->json(['error' => 'Unauthorized'], 401);
+// 		}
+// 	}
+// 	/**
+// 	 * admin register API
+// 	 * @return \Illuminate\Http\Response
+// 	 */
+// 	public function adminRegister(Request $request)
+// 	{
+// 		$validator = Validator::make($request->all(), [
+// 			'name' => 'required',
+// 			'email' => 'required|email',
+// 			'password' => 'required',
+// 			'c_password' => 'required|same:password',
+// 		]);
+// 		if ($validator->fails()) {
 			
-			return response()->json($validator->errors(), 417);
-		}
-		$user = User::create([
-			'name' => $request->name,
-			'email' => $request->email,
-			'password' => bcrypt($request->password),
-		]);
-		$success['name'] = $user->name;
-		$success['token'] = $user->createToken('MyApp', ['*'])->accessToken;
-		return response()->json(['success' => $success], 200);
-	}
+// 			return response()->json($validator->errors(), 417);
+// 		}
+// 		$user = User::create([
+// 			'name' => $request->name,
+// 			'email' => $request->email,
+// 			'password' => bcrypt($request->password),
+// 		]);
+// 		$success['name'] = $user->name;
+// 		$success['token'] = $user->createToken('MyApp', ['*'])->accessToken;
+// 		return response()->json(['success' => $success], 200);
+// 	}
 }
